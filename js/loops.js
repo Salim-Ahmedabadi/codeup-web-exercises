4350e4e34e9c07770595c5d7d32011eb73e06a51
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-02-11 16:50:30 -0600

    feat: Added For Loops Exercise

--- js/loops.js
+++ js/loops.js
@@ -1,37 +1,10 @@
-//Loops lecture
-var areWeThereYet;
 
-while(!areWeThereYet){
-    areWeThereYet = confirm("Are we there yet?")
-}
-var haveToy = confirm("Can I have a toy please");
+function showMultiplicationTable () {
+    for (var i = 1; i <= 10; i++) {
+        var result = i * 7;
+        console.log("7 * " + i + " = " + result);
+    }
 
-while(!haveToy){
-    haveToy = confirm("Please")
-}
-if(haveToy){
-    alert("Thank you!!!")
-}
-var waitingForToy = confirm("Are we still waiting for his toy");
+    }
+    showMultiplicationTable();
 
-while(waitingForToy){
-    waitingForToy = confirm("How about now")
-}
-if(!waitingForToy){
-    alert("Thank you!!!")
-}
-var x = 0;
-while (x < 10){
-    x++;
-    console.log(x)
-}
-var y = 10;
-while (y > 0) {
-    y--;
-    console.log(y)
-}
-var z = 10;
-while (z > 0) {
-    //z--;
-    console.log(z--)
-}
