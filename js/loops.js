d5c376f5371260a946e4b070ab048fa1b3b850c0
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-02-11 20:07:11 -0600

    feat: Added For Loops Exercise

--- js/loops.js
+++ js/loops.js
@@ -8,3 +8,13 @@ function showMultiplicationTable () {
     }
     showMultiplicationTable();
 
+for( var i = 1 ; i <=10 ; i++){
+    var random = Math.floor((Math.random() * 200) + 20)
+    if(random % 2 === 0){
+        console.log(random + " is even");
+    }else{
+        console.log( random  + " is odd");
+    }
+}
+
+
