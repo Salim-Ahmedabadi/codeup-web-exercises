c1536ff08ca6a0434c08a0f67e9a346ba8357c80
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-03-15 14:22:39 -0500

    feat: Started orking on Flexbox Practice Exercise

--- js/Javascript101.js
+++ js/Javascript101.js
@@ -2,3 +2,7 @@
 // On the line below, create a variable named onMarsRightNow and assign it the boolean value of false
 var onMarsRightNow = false;
 console.log(onMarsRightNow);
+
+// Exercise 2
+// For more on arrays, see https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Arrays
+// Create a variable named fruits and assign it an array of strings containing the following fruits.
