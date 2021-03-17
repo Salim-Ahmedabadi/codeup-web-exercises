81144fcbf16d33708d3ee97f0ebca0aa5c78dbb9
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-03-17 16:16:51 -0500

    feat: WSorking on JavaScript 101 Exercises to practice more

--- js/Javascript101.js
+++ js/Javascript101.js
@@ -1,8 +1,42 @@
 //  Exercise 1
 // On the line below, create a variable named onMarsRightNow and assign it the boolean value of false
-var onMarsRightNow = false;
-console.log(onMarsRightNow);
+    var onMarsRightNow = false;
+    console.log(onMarsRightNow);
 
 // Exercise 2
 // For more on arrays, see https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Arrays
 // Create a variable named fruits and assign it an array of strings containing the following fruits.
+// mango, banana, guava, kiwi, and strawberry.
+    var fruits = ["mango","banana","guava","kiwi","strawberry"];
+    console.log(fruits);
+
+// Exercise 3
+// Create a variable named vegetables and assign it an array of strings containing the following vegetables.
+// eggplant, broccoli, carrot, cauliflower, and zucchini
+    var vegetables = ["eggplant","broccoli","carrot","cauliflower","zucchini"];
+    console.log(vegetables);
+
+//Exercise 4
+// Create a variable named numbers and assign it an array of numbers, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+console.log(numbers);
+
+//Exercise 5
+// Add the string "tomato" to the end of the fruits array.
+// *Hint* Recommend finding and using a built-in JS operation to add to an array rather than recreating the array.
+fruits.push("tomato");
+console.log(fruits);
+
+// Exercise 6
+// add the string "tomato" onto the end of the vegetables array.
+// Recommend using the built-in JS operation to add to an array.
+vegetables.push("tomato");
+console.log(vegetables);
+
+// Exercise 7
+// Given the array of numbers defined below, reverse the array of numbers that you created above.
+var someNumbers = [1,2,3,4,5,6,7,8,9,10];
+someNumbers.reverse();
+console.log(someNumbers);
+
+
