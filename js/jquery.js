bf1502152c1554363b4e8f14ebdbfaf36bfc723c
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-04-06 12:15:52 -0500

    feat: Finished Mouse Events Exercise

--- js/jquery.js
+++ js/jquery.js
@@ -3,9 +3,26 @@
 // });
 //
 // console.log("hello jQuery!");
-    $(document).ready(function() {
-        $('.important').css('background-color', 'skyblue');
-        $('.important').html('Salim Rocks');
-
-
-    });
+//     $(document).ready(function() {
+//         $('.important').css('background-color', 'skyblue');
+//         $('.important').html('Salim Rocks');
+//
+//
+//     });
+$('h1').click(function(e){
+    $('h1').css({'background-color': 'red'})
+    $('h1').html('I just clicked it')
+    console.log(e);
+})
+$('p').dblclick(function(e){
+    $('p').css({'font-weight': '18px'})
+    $('p').html('Too much clicking')
+    console.log(e);
+})
+$('li').hover(function(e){
+    console.log('enter');
+    $(e.target).css('background', 'red')
+},function(e){
+    console.log('leave')
+    $(e.target).css('background', 'black')
+})
