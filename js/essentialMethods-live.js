b3e41e4c5a9f5c95c4f435480d3abc7bb9604364
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-04-07 16:16:01 -0500

    feat: Finished essentialMethods-live.JS Exercise

--- js/essentialMethods-live.js
+++ js/essentialMethods-live.js
@@ -6,12 +6,17 @@
 $("#best-seller-cont").html();
 //TODO together: Let's set up a string with HTML to replace this old list with our updated best sellers - FEDORAS have moved into the number one spot!
 var html = "";
-html = "<h4 class=\"best-seller-header\">Our Top Sellers</h4>" +
-    "<ol><li>Fedora Hats</li>" +
-    "<li>Baseball Hats</li>" +
-    "<li>Cowboy Hats</li></ol>";
+html = "<h4 class=\"best-seller-header\">Our Top Sellers</h4>"
+html += "<ol><li>Fedora Hats</li>";
+html += "<li>Baseball Hats</li>";
+html += "<li>Cowboy Hats</li></ol>";
+html += "<ol><li class='hat-sold'>Fedora Hats</li>";
+html += "<li class='hat-sold'>Baseball Hats</li>";
+html += "<li class='hat-sold'>Cowboy Hats</li></ol>";
+
 //TODO: Now, let's get back into that "best-seller-cont" and replace that HTML!
 $("#best-seller-cont").html(html)
+
 //TODO Together: Can we make a click event that changes "Howell's" to "Marco's" when we click on it? Help me out!
 $("navbar-brand").click(function(){
     $(this).html("Marco's <i class=\"bi-shop mx-1\"></i>");
@@ -59,6 +64,7 @@ console.log(navcontent);
 $("nav").addClass("navbar-light, bg-info")
 //TODO: Finally, create the following - add your own button somewhere on the navbar. Add an event that will toggle the navbar between bg-dark/navbar-dark and bg-info/navbar-light respectively
 
+
 //TODO: Create your own object with multiple CSS properties you'd like to change. Change whatever element(s) you had in mind with your CSS object and .css()!
 
 
@@ -66,19 +72,27 @@ $("nav").addClass("navbar-light, bg-info")
 //traversing
 
 //.each() - iterate over each matched element to do some stuff
-//TODO: Let's change the last two lis of the class .hat-sold to "Baseball hats" as well
-
+//TODO: Let's change the last two lis of the class .hat-sold to "Fedora hats" as well
+$(".hat-sold").each(function(index, ele) {
+    if(index !== 0){
+        $(ele).html("Fedora hats");
+    }
+})
 //.first()
 //TODO: Let's change the first list item in the class of .hat-sold from "Baseball hats" to "Porkpie hats"
+$(".hat-sold").first().html("Crazy hats");
 
 //.last()
 //TODO Together: Let's change the last li in the class of .hat-sold to be Fancy hats - italicized! ! !
+$(".hat-sold").last().html("<em>Fancy hats</em>");
 
 //.parent()
 //TODO: Let's pop up the tree to the parent (<ol>) and use .css to change the list-style-type
+$(".hat-sold").parent().css("list-style-type", "upper-roman")
 
 //.children()
 //TODO Together: What ID would I need to target to see all of the HTML, as children, that we've been manipulating so far?
-
+console.log($("#best-seller-cont").children());
 //.next()
 //TODO: Let's slide across branches (sibling to sibling) on our tree and change the HTML within the ordered list we find there to only show Top Hats! !
+$(".best-seller-header").next().html("<li>Top Hat</li>")
