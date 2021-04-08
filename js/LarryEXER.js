9b1dd9270b985eead313910269b65ac42201c29d
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-04-08 12:25:28 -0500

    feat: Added jquery-ajax-lecture

---
+++ js/LarryEXER.js
@@ -0,0 +1,33 @@
+// When a user hits the "g" key, the background of the page should turn purple.
+var counter = 0;
+var arr = [];
+var solution = [49,50,51];
+
+        $(document).keyup(function(event){
+             console.log(event.keyCode);
+            arr.push(event.keyCode)
+            if (event.keyCode === 71){
+                $("body").css("background-color" , "purple");
+
+            }
+            if (event.keyCode === 13){
+                counter += 1;
+                console.log(counter);
+            }
+
+                if(arr.join(",") === solution.join(",")){
+                    alert("You get a 50% discount");
+                }
+        })
+
+//     Every time the enter key is pressed, increment and display a counter that shows the total number of times the enter key has been pressed.
+
+
+
+
+//     When someone types 1, 2, 3 in a row, show an alert that says "You get a 50% discount!".
+
+hhhhhhhhhhhhhahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahhahahahahahahahahahahahaha
+
+
+
