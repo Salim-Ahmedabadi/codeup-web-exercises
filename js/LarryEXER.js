c3e32bc0d9be9096afbdd8cb90e231a255f48fd9
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-04-15 11:29:08 -0500

    feat: Started working on es6 lecture

--- js/LarryEXER.js
+++ js/LarryEXER.js
@@ -1,8 +1,8 @@
 // When a user hits the "g" key, the background of the page should turn purple.
-var counter = 0;
+// var counter = 0;
+// var arr = [];
+// var solution = [49,50,51];
 var arr = [];
-var solution = [49,50,51];
-
         $(document).keyup(function(event){
              console.log(event.keyCode);
             arr.push(event.keyCode)
@@ -10,24 +10,31 @@ var solution = [49,50,51];
                 $("body").css("background-color" , "purple");
 
             }
-            if (event.keyCode === 13){
-                counter += 1;
-                console.log(counter);
-            }
-
-                if(arr.join(",") === solution.join(",")){
-                    alert("You get a 50% discount");
-                }
-        })
+        //     if (event.keyCode === 13){
+        //         counter += 1;
+        //         console.log(counter);
+        //     }
+        //
+        //         if(arr.join(",") === solution.join(",")){
+        //             alert("You get a 50% discount");
+        //         }
+        // })
 
 //     Every time the enter key is pressed, increment and display a counter that shows the total number of times the enter key has been pressed.
-
-
+            var counter = 0;
+if (event.keyCode === 13){
+    counter += 1;
+    console.log(counter);
+}
 
 
 //     When someone types 1, 2, 3 in a row, show an alert that says "You get a 50% discount!".
+            var solution = [49,50,51];
+            if(arr.join(",") === solution.join(",")){
+    alert("You get a 50% discount");
+}
+})
 
-hhhhhhhhhhhhhahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahhahahahahahahahahahahahaha
 
 
 
