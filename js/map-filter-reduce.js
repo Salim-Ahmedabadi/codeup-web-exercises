ad80b793c8eb945c07e483e12f580e2f0c42fa5a
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-04-16 17:15:05 -0500

    feat: Pending last 2 in the map-filter-reduce exercise

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,62 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+//2.Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
+const threeLanguageUsers = users.filter(user => user.languages.length >= 3);
+console.log(threeLanguageUsers);
+
+//3. Use .map to create an array of strings where each element is a user's email address
+const emailListArray = users.map(user => user.email);
+console.log(emailListArray);
+
+//4. Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
+const totalYears = users.reduce((total, user) => total + user.yearsOfExperience, 0);
+console.log(totalYears)
+
+//5. Use .reduce to get the longest email from the list of users.
+let longestEmails = users.reduce(function(longEmail, name){
+    longEmail[name.email] = name.email.length;
+    return longEmail;
+},[]);
+console.log(longestEmails);
+
+//6. Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+
+
+//Bonus -Use .reduce to get the unique list of languages from the list of users.
+
