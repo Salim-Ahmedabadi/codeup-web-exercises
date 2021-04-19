944f44252bb62391da57fe8fe566e9e841ee7593
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-04-19 15:33:27 -0500

    feat: Added code after review or the promises exercise

--- js/promises.js
+++ js/promises.js
@@ -23,3 +23,23 @@ console.log(wait()); // pending promise
 
 wait(1000).then((num) => console.log(`You\'ll see this after ${num} ms`));
 wait(3000).then((num) => console.log(`You\'ll see this after ${num} ms`));
+//Douglas Review Code below
+// function getLastPushDate(githubUsername) {
+//     let url = `https://api.github.com/users/${githubUsername}/events`;
+//     let githubResponse = fetch(url, {headers: {'Authorization': `token ${GITHUB_PAT}`}});
+//     return githubResponse
+//         .then((response)=>{
+//             return response.json();
+//         })
+//         .then((githubEventData)=>{
+//             for (let githubEvent of githubEventData) {
+//                 if(githubEvent.type === "PushEvent") {
+//                     console.log(githubEvent.created_at);
+//                     return new Date(githubEvent.created_at);
+//                 }
+//             }
+//         });
+// }
+//
+// getLastPushDate("douglas-codeup")
+//     .then((date)=>console.log(date));
