c3317c9a401e1e35b5caf56b34211b9f2c916e71
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-06-14 22:24:49 -0500

    Modified JS101 Exercises

--- js/Javascript101.js
+++ js/Javascript101.js
@@ -450,3 +450,16 @@ function mean (arr) {
     });
     return count / arr.length
 }
+// Exercise 62
+// Write a function definition named median that takes in sequence of numbers and returns the average value
+function median(values) {
+
+    values.sort( function(a,b) {return a - b;} );
+
+    var half = Math.floor(values.length/2);
+
+    if(values.length % 2)
+        return values[half];
+    else
+        return (values[half-1] + values[half]) / 2.0;
+}
